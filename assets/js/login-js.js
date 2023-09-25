7b1e4ddd58d17d293cbe60a17b5bf66a73e7ea75
Sixtus32 <sixtuswork18@gmail.com>
2023-09-25 15:11:04 +0200

    email inpuut shake effect added

--- assets/js/login-js.js
+++ assets/js/login-js.js
@@ -1,2 +1,25 @@
 // import { lock2fill } from 'remixicon/icons';
 // import { maillfill } from 'remixicon/icons';
+
+//obtencion del elemento del campo de entrada y el boton
+document.addEventListener("DOMContentLoaded", () => {
+const emailInput=document.getElementById("emailInput");
+const passwordInput=document.getElementById("passwordInput")
+const loginBtn=document.getElementById("loginbtn");
+
+loginBtn.addEventListener("click",() => {
+    const emailValue=emailInput.value;
+
+    if(!isValidEmail(emailValue)){
+        emailInput.classList.add("shake");
+
+        setTimeout(() => {
+            emailInput.classList.remove("shake");
+        }, 1000);
+    }
+})
+
+const isValidEmail= (email) => {
+    return email.length > 0 && email.includes("@") && email.includes(".");
+}
+});
